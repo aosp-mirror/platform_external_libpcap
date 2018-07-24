165065aed040e9658cb43fc1194d6799c9227043
Haibo Huang <hhb@google.com>
2018-07-23 17:26:52 -0700

    Update libpcap to 1.9.0
    Test: compile
    Change-Id: I5b036bc4544b3150629b943d3a7896d7dc03fec9

--- grammar.h
+++ grammar.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.2.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -289,10 +289,10 @@ extern int pcap_debug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE YYSTYPE;
+
 union YYSTYPE
 {
-#line 271 "grammar.y" /* yacc.c:1909  */
+#line 286 "grammar.y" /* yacc.c:1909  */
 
 	int i;
 	bpf_u_int32 h;
@@ -310,6 +310,8 @@ union YYSTYPE
 
 #line 312 "grammar.h" /* yacc.c:1909  */
 };
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
 #endif
